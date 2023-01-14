8f87b563bad7d604f8d6e94dbf83b3d87637322c
Dtech7 <neild.felix@gmail.com>
2023-01-13 20:16:29 -0500

    initial setup for springboot backend

---
+++ ERS-V2/back-end/ers-v2-backend/src/main/java/com/ersv2/ErsV2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ersv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsV2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsV2BackendApplication.class, args);
+	}
+
+}
